527104dd208113e4e0d17dc8af3229fe71eccd5c
Dhanashree Sagane <dnsagane007@gmail.com>
2023-08-01 21:21:23 +0530

    MANAS react project created

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container-fluid">
+      This is a homepage where Rushi needs to code
+    </div>
+  );
+}
+
+export default Home;
